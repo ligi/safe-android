63bf0c41c95ecf734d9b28e5f0efdb509782a551
Vitaly Katz <vitaly@gnosis.pm>
2020-09-17 12:18:55 +0200

    Revert to - as build number separator

--- ci/prepare_env_buildkite.sh
+++ ci/prepare_env_buildkite.sh
@@ -9,7 +9,7 @@ if [[ $BUILDKITE_BRANCH  =~ ^v[0-9]+.* ]]; then
     export APP_VERSION_NAME=${BUILDKITE_BRANCH:1}
 else
   version="$(./gradlew -q pV | tail -1)"
-  export APP_VERSION_NAME="${version}.${APP_VERSION_CODE}"
+  export APP_VERSION_NAME="${version}-${APP_VERSION_CODE}"
 fi
 
 export APP_RELEASE_NOTES=$BUILDKITE_MESSAGE
